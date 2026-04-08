4a96fe166d47cd54b761d2be256d2092c32c1b3f
samian <sam.iannazzo@yahoo.com>
2026-04-08 14:25:52 -0700

    created controller

---
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment3.Controllers
+{
+    [Route("event")]
+    public class EventController : Controller
+    {
+        [HttpGet("event/register/{eventCode}")]
+
+        public IActionResult eventCode()
+        {
+            return View();
+        }
+    }
+}
