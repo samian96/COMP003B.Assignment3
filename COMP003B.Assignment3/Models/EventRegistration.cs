98040afd091e49eb7d45574b8510c95337646287
samian <sam.iannazzo@yahoo.com>
2026-04-07 20:47:07 -0700

    created eventregistration model with required fields

---
+++ COMP003B.Assignment3/Models/EventRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.Assignment3.Models
+{
+    public class EventRegistration
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(6)]
+
+        public string EventCode { get; set }
+
+        [Required]
+        [Range(1,10)]
+
+        public int Tickets { get; set; }
+
+    }
+}
