c22500be60ee696224706d27037e64ae8f4bff2b
samian <sam.iannazzo@yahoo.com>
2026-04-08 14:37:50 -0700

    added FromForm as placeholder for now as unsure it will work

--- COMP003B.Assignment3/Controllers/EventController.cs
+++ COMP003B.Assignment3/Controllers/EventController.cs
@@ -1,15 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using COMP003B.Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace COMP003B.Assignment3.Controllers
 {
     [Route("event")]
     public class EventController : Controller
     {
-        [HttpGet("event/register/{eventCode}")]
+        [HttpGet("register/{eventCode}")]
 
-        public IActionResult eventCode()
+        public IActionResult Register(string eventCode)
         {
             return View();
+        }
+
+        [HttpPost("register/{eventCode}")]
+
+        public IActionResult Register([FromForm] EventRegistration registration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registration);
+            }
+
+            return RedirectToAction("Success", registration);
         }
     }
 }
