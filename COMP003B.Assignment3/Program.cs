f81191342f393b74ada247353ce36f3415f5f681
samian <sam.iannazzo@yahoo.com>
2026-04-07 20:21:55 -0700

    Add project files.

---
+++ COMP003B.Assignment3/Program.cs
@@ -0,0 +1,42 @@
+/* Samuel David Iannazzo
+ * COMP-003B: ASP.NET Core
+ * Faculty: Johnathan Cruz
+ * The purpose of this application is to show my understanding of how to use routing, model binding, as well as model validation is used properly.
+ */
+
+namespace COMP003B.Assignment3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
